3c09d17f7c7b2e1bcb752de0656ef4aab2d0dfbc
Daniel Pinheiro <lopesdanielhps@gmail.com>
2026-04-22 17:48:57 -0300

    db.ts alterado para istancia voltar a funcionar

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -9,10 +9,9 @@ if (!process.env.DATABASE_URL) {
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function testDbConnection() {
